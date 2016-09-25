5fa318c26dbcdf53ec639f64b63118232c17b334
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2016-09-25 16:03:14 -0700

    I am awesome

---
+++ src/zodiac_signs.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+
+public class zodiac_signs {
+public static void main(String[] args) {
+	String symbol  =JOptionPane.showInputDialog("what is your zodiac vladmere ?");
+ if(symbol.equals("aries")){
+	 JOptionPane.showMessageDialog(null , "the cockroaches are waiting for you");			
+ 
+	 if(symbol.equals("leo")) 
+	
+	 
+JOptionPane.showMessageDialog(null,"is drinking one of your bad habits?");
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ }
+
+}	
+

2c0a0d7bd226b2f169fe193b83b0a69e7a8cc337
Anthony Cerruti <aprime2003@gmail.com>
2017-02-19 11:29:56 -0800

    dsfhla
    jtdcthj

---
+++ src/com/sun/java/swing/plaf/gtk/resources/metacity/SwingFallbackTheme/Madlibs.java
@@ -0,0 +1,32 @@
+package com.sun.java.swing.plaf.gtk.resources.metacity.SwingFallbackTheme;
+
+import javax.swing.JOptionPane;
+
+public class Madlibs {
+	public static void main(String[] args) {
+		String noun ; 
+		String verb;
+		String adjective;
+	adjective=JOptionPane.showInputDialog("enter an adjective");	
+noun=JOptionPane.showInputDialog("enter a noun");
+verb= JOptionPane.showInputDialog( "enter a verb");
+
+JOptionPane.showMessageDialog(null, adjective+noun+verb );
+JOptionPane.showMessageDialog(null, "Thankyou for your feedback");
+/** Copyright The League of Amazing Programmers 2013-2017
+	Level 0
+	Madlibs
+	Duration=.00
+	Platform=Eclipse
+	Type=Instructions
+	Objectives=JOptionPane.showInpuDialog & Message Dialog & \n
+	Optional=Yes
+	*/
+
+	/** 
+	Create a story and ask the user for different adjectives, nouns, verbs etc. and fill it in to your story.		
+	*/
+
+														
+
+	}}
---
+++ src/com/sun/java/swing/plaf/gtk/resources/metacity/SwingFallbackTheme/PracticeString.java
@@ -0,0 +1,16 @@
+package com.sun.java.swing.plaf.gtk.resources.metacity.SwingFallbackTheme;
+
+public class PracticeString {
+public static void main(String[] args) {
+	String jackie = "Your name";
+	String dylan = "Dylan is the best at everything and has an ego says mcpuffercorn";
+
+System.out.println(jackie+dylan);
+
+
+
+
+
+
+}}
+	

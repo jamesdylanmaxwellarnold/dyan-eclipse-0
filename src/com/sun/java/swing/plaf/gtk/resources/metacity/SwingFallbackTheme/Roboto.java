2225b3e81c0d958658d92c2e08003135b54110a8
Love4653 <league@iMac.attlocal.net>
2017-05-12 16:51:23 -0700

    hbrgts
    rgyh

---
+++ src/com/sun/java/swing/plaf/gtk/resources/metacity/SwingFallbackTheme/Roboto.java
@@ -0,0 +1,42 @@
+package com.sun.java.swing.plaf.gtk.resources.metacity.SwingFallbackTheme;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Roboto {
+
+ public static void main(String[] args) {
+	Robot jeff = new Robot();
+jeff.setSpeed(10);	
+ jeff.penDown();
+ jeff.turn(270);
+jeff.move(500);
+ jeff.turn(90);
+for (int i = 0; i < 4; i++) {
+	
+
+ 
+ jeff.move(500);
+ jeff.turn(90);
+ jeff.move(50);
+ jeff.turn(90);
+ jeff.move(500);
+ jeff.turn(270);
+ jeff.move(10);
+ jeff.turn(270);
+ jeff.move(400);
+ jeff.turn(90);
+ jeff.move(50);
+ jeff.turn(90);
+ jeff.move(400);
+ jeff.turn(270);
+ jeff.move(20);
+ jeff.turn(270);
+ jeff.move(450);
+ jeff.turn(90);
+ jeff.move(50);
+ jeff.turn(90);
+ jeff.move(450);
+ jeff.turn(270);
+ jeff.move(50);
+ jeff.turn(270);
+} } } 
